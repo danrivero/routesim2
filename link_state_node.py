f51e9f9c3a47e44cb3db1d1d9b470343f1de2153
danrivero <DanielRivero2026@u.northwestern.edu>
2025-02-25 22:43:42 -0500

    draft 1

--- link_state_node.py
+++ link_state_node.py
@@ -1,5 +1,6 @@
 from simulator.node import Node, Link
 import json
+import heapq
 
 class Link_State_Node(Node):
 
@@ -17,13 +18,17 @@ class Link_State_Node(Node):
     # Fill in this function
     def link_has_been_updated(self, neighbor, latency):
         # latency = -1 if delete a link
+        if latency == -1:
+            cond_lat = float('inf')
+        else:
+            cond_lat = latency
         # when link is updated, propagate new self information to neighbors
         # json message includes <source, destination, sequence number, and latency>
         msg_body = {
             "src": self.id,
             "dst": neighbor,
             "seq_num": self.seq_num,
-            "lat": latency
+            "lat": cond_lat
         }
         msg_to_send = json.dumps(msg_body)
         self.seq_num += 1
@@ -33,15 +38,42 @@ class Link_State_Node(Node):
     def process_incoming_routing_message(self, m):
         # when receiving updated info, forward it to all neighbors
         msg = json.load(m)
-        if not self.node_messages["src"] or self.node_messages["src"] <= msg["seq_num"]:
-            # add to graph
-            self.router_graph
+        if not self.node_messages[msg["src"]] or self.node_messages[msg["src"]] <= msg["seq_num"]:
+            self.node_messages[msg["src"]] = msg["seq_num"]
+
+            self.router_graph[(msg["src"], msg["dst"])] = msg["lat"]
+            self.router_graph[(msg["dst"], msg["src"])] = msg["lat"]
+
             self.send_to_neighbors(m)
         
 
     # Return a neighbor, -1 if no path to destination
     def get_next_hop(self, destination):
         return -1
+
+    def shortest_path(self, src):
+        dsts = {}
+        prvs = {}
+
+        pq = []
+        
+        for node in self.router_graph:
+            dsts[node] = float('inf')
+            heapq.heappush(pq, (float('inf'), node))
+            prvs[node] = None
+
+        dsts[self.id] = 0
+
+        while pq:
+            _, node = heapq.heappop(pq)
+            for neighbor in self.neighbors:
+                alt = dsts[node] + self.router_graph[node][neighbor]
+                if alt < dsts[neighbor]:
+                    dsts[neighbor] = alt
+                    prvs[neighbor] = node
+            
+        return dsts, prvs
+        
 
 # 
 #
