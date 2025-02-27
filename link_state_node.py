c490f8b912eb9382c2a5458302e7fb64536465df
justinsortland20 <108437799+justinsortland20@users.noreply.github.com>
2025-02-27 14:12:51 -0600

    Optimized link_state_node.py

--- link_state_node.py
+++ link_state_node.py
@@ -8,6 +8,7 @@ class Link_State_Node(Node):
         super().__init__(id)
         self.router_graph = {} # adj matrix - key: (src, dst) -> val: cost
         self.node_messages = {} # key: node id, val: seq num
+        self.adj_list = {} # key: node (int), val: list of nodes (ints)
         self.seq_num = 0
 
     # Return a string
@@ -23,15 +24,29 @@ class Link_State_Node(Node):
         # latency = -1 if delete a link
 
         if latency == -1:
-            self.neighbors.remove(neighbor)
-            cond_lat = float('inf')
-        else:
-            if neighbor not in self.neighbors:
-                self.neighbors.append(neighbor)
-            cond_lat = latency
+            if neighbor in self.neighbors:
+                self.neighbors.remove(neighbor)
 
-        self.router_graph[f"{self.id},{neighbor}"] = cond_lat
-        self.router_graph[f"{neighbor},{self.id}"] = cond_lat
+            self.router_graph.pop(f"{self.id},{neighbor}", None)
+            self.router_graph.pop(f"{neighbor},{self.id}", None)
+
+            msg_body = {
+                "router_graph": self.router_graph,
+                "origin": self.id,
+                "seq_num": self.seq_num,
+                "deleted_link": f"{self.id},{neighbor}"
+            }
+
+            self.seq_num += 1
+            msg_to_send = json.dumps(msg_body)
+            self.send_to_neighbors(msg_to_send)
+            return
+        
+        if neighbor not in self.neighbors:
+            self.neighbors.append(neighbor)
+
+        self.router_graph[f"{self.id},{neighbor}"] = latency
+        self.router_graph[f"{neighbor},{self.id}"] = latency
 
         msg_body = {
             "router_graph": self.router_graph,
@@ -40,7 +55,6 @@ class Link_State_Node(Node):
         }
 
         self.seq_num += 1
-
         msg_to_send = json.dumps(msg_body)
         self.send_to_neighbors(msg_to_send)
 
@@ -48,41 +62,45 @@ class Link_State_Node(Node):
     def process_incoming_routing_message(self, m):
         # when receiving updated info, forward it to all neighbors
         msg = json.loads(m)
-        # print(str(self.id) + " received a message from " + str(msg["origin"]))
         routing_table = msg["router_graph"]
         origin = msg["origin"]
         seq_num = msg["seq_num"]
 
-        for edge in routing_table:
-            src, dst = map(int, edge.split(","))
-            self.router_graph[f"{src},{dst}"] = routing_table[edge]
-            self.router_graph[f"{dst},{src}"] = routing_table[edge]
+        # self.router_graph.update(routing_table)
 
-        # if self.id in msg["visited"]: 
-        #     return
-
-        # new_visited = msg["visited"].copy()
-        # new_visited.append(self.id)
-            
-        # msg_body = {
-        #     "src": msg["src"],
-        #     "dst": msg["dst"],
-        #     "visited": new_visited,
-        #     "lat": msg["lat"]
-        # }
         if origin in self.node_messages and seq_num <= self.node_messages[origin]:
             return
-        
         self.node_messages[origin] = seq_num
 
-        msg_body = {
-            "router_graph": self.router_graph,
-            "origin": origin,
-            "seq_num": seq_num
-        }
-        
-        send = json.dumps(msg_body)
-        self.send_to_neighbors(send)
+        if "deleted_link" in msg:
+            edge = msg["deleted_link"]
+            src, dst = map(int, edge.split(","))
+            if f"{src},{dst}" in self.router_graph and f"{dst},{src}" in self.router_graph:
+                del self.router_graph[f"{src},{dst}"]
+                del self.router_graph[f"{dst},{src}"]
+
+            msg_body = {
+                "router_graph": self.router_graph,
+                "origin": origin,
+                "seq_num": seq_num,
+                "deleted_link": edge
+            }
+            send = json.dumps(msg_body)
+            self.send_to_neighbors(send)
+            return
+
+        prev_graph_state = self.router_graph.copy()
+        self.router_graph.update(routing_table)
+
+        if prev_graph_state != self.router_graph and origin != self.id:
+            msg_body = {
+                "router_graph": self.router_graph,
+                "origin": origin,
+                "seq_num": seq_num
+            }
+            
+            send = json.dumps(msg_body)
+            self.send_to_neighbors(send)
 
         # print("this is the current graph of " + str(self.id))
         # print(self.router_graph)
@@ -114,7 +132,7 @@ class Link_State_Node(Node):
             return -1
         
         curr = destination
-        while prvs[curr] is not self.id:
+        while prvs[curr] != self.id:
             curr = prvs[curr]
             # print("get next hop")
         return curr
@@ -129,7 +147,6 @@ class Link_State_Node(Node):
             src, dst = map(int, edge.split(","))
             vertices.add(src)
             vertices.add(dst) 
-        vertices = list(vertices)
 
         for vertex in vertices:
             dsts[vertex] = float('inf')
@@ -157,7 +174,7 @@ class Link_State_Node(Node):
                     min_neighbors.add(dst)
                 elif dst == min_vertex:
                     min_neighbors.add(src)
-            min_neighbors = list(min_neighbors)
+            # min_neighbors = list(min_neighbors)
             #print("**")
             #print(f"min neighbors: {min_neighbors}")
             
@@ -168,14 +185,20 @@ class Link_State_Node(Node):
                     #print(alt)
                     dsts[neighbor] = alt
 
-                    i = 0
-                    for tuple in pq:
-                        _, vertex = tuple
-                        if vertex == neighbor:
+                    # i = 0
+                    # for tuple in pq:
+                    #     _, vertex = tuple
+                    #     if vertex == neighbor:
+                    #         break
+                    #     i += 1
+                    # pq[i][0] = alt
+                    # heapq.heapify(pq)
+                    for i in range(len(pq)):
+                        if pq[i][1] == neighbor:
+                            pq[i][0] = alt
                             break
-                        i += 1
-                    pq[i][0] = alt
                     heapq.heapify(pq)
+
 
                     prvs[neighbor] = min_vertex
             
